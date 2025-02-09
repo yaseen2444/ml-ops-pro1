83efaa64b2ea8e647eebe786a653a3ba72a8398f
yaseen2444 <yaseenmohammadap37@gmail.com>
2025-02-09 14:31:03 +0530

    structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="YASEEN MD",
+    author_email="yaseenmohammadap37@gmail.com",
+    packages=find_packages()
+)
